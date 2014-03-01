ce424cb3ef23df9326d0baeb97d3be1d9f7bd237
王千一 <xdushepherd@gmail.com>
2014-03-01 10:11:20 +0800

    开始大型仪器共享平台开发

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Platform::Application.config.secret_key_base = 'bcce84c824f4c922babb94b8764b6b9a6fcd69f32ca2ee9fe05c6a490b6aafa89885c70c0e1e61bcf56243acf24c6adb84889302859086a4d586eba9da3544ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Platform::Application.config.session_store :cookie_store, key: '_platform_session'
